2788b5af58ab895fd4eaeca631853c67581c68bd
Yash Chauhan <yashchauhan125dpr@gmail.com>
2026-04-14 12:41:29 +0530

    Update CORS origin to allow new IP address

--- services/video-services/src/server.js
+++ services/video-services/src/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  origin: ['http://localhost:3000', 'http://3.7.114.115:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
