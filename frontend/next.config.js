c473f568319486b7362b5057ec622e528c6bedac
Your Name <your.email@example.com>
2026-04-15 13:05:38 +0530

    change

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,18 +10,18 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ['localhost', 'your-s3-bucket.s3.amazonaws.com'],
+    domains: ['yash-demo-s3-bucket-6660.s3.ap-south-1.amazonaws.com', 'img.youtube.com', 'placehold.co', 'image.tmdb.org'],
     unoptimized: true
   },
   env: {
-    API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:5000',
-    STREAMING_BASE_URL: process.env.STREAMING_BASE_URL || 'http://localhost:4567'
+    API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://3.7.114.115:5000',
+    STREAMING_BASE_URL: process.env.NEXT_PUBLIC_STREAMING_BASE_URL || 'http://3.7.114.115:5000/api/stream'
   },
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.API_BASE_URL || 'http://localhost:5000'}/api/:path*`
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL || 'http://3.7.114.115:5000'}/api/:path*`
       }
     ];
   }
